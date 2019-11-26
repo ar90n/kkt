c164b4a9d071aad622e1c152b6a3fa85b2b7aa9e
Masahiro Wada <argon.argon.argon@gmail.com>
2019-11-26 01:38:21 +0000

    Imporve script template

--- kaggle_kernel_tool/builders/kernels/script_kernel.py
+++ kaggle_kernel_tool/builders/kernels/script_kernel.py
@@ -1,23 +1,20 @@
 SCRIPT_TEMPLATE: str = """def __bootstrap__():
+    import sys
     import base64
     import gzip
     import os
     from pathlib import Path
-    from typing import Dict
     from tempfile import TemporaryDirectory
 
-    def run(command):
-        os.system("export PYTHONPATH=${{PYTHONPATH}}:/kaggle/working && " + command)
-
     # this is base64 encoded source code
-    pkg_name: str = "{pkg_name}"
     pkg_encoded: str = "{pkg_encoded}"
 
     with TemporaryDirectory() as temp_dir:
-        pkg_path = Path(temp_dir) / pkg_name
+        pkg_path = Path(temp_dir) / "{pkg_name}"
         pkg_path.write_bytes(gzip.decompress(base64.b64decode(pkg_encoded)))
-        run("pip install --no-deps {{pkg_path}} -t /kaggle/working".format(pkg_path=pkg_path))
+        os.system("pip install {{pkg_path}} -t /kaggle/working".format(pkg_path=pkg_path))
 
+    sys.path.append("/kaggle/working")
 __bootstrap__()
 
 {kernel_body}
