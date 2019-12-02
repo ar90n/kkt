2206c7cabfc13c8c1372f521afb239fb5887eb4b
Masahiro Wada <argon.argon.argon@gmail.com>
2019-12-02 16:42:49 +0000

    Revert "Add --no-deps when installing packages"
    This reverts commit e0af2b3370a99c4bc61036c4a12449dd7ba1b3b4.

--- kkt/builders/kernels/notebook_kernel.py
+++ kkt/builders/kernels/notebook_kernel.py
@@ -11,8 +11,12 @@ def erace_all_outputs(notebook: dict):
     return notebook
 
 
-def create_bootstrap_cell(pkg_name: str, pkg_encoded: str):
-    bootstrap_code = create_bootstrap_code(pkg_name=pkg_name, pkg_encoded=pkg_encoded)
+def create_bootstrap_cell(
+    pkg_name: str, pkg_encoded: str, enable_internet: bool = False
+):
+    bootstrap_code = create_bootstrap_code(
+        pkg_name=pkg_name, pkg_encoded=pkg_encoded, enable_internet=enable_internet
+    )
     return {
         "cell_type": "code",
         "execution_count": None,
@@ -22,12 +26,18 @@ def create_bootstrap_cell(pkg_name: str, pkg_encoded: str):
     }
 
 
-def create_notebook_kernel(notebook_body: str, pkg_name: str, pkg_encoded: str):
-    bootstrap_code = create_bootstrap_code(pkg_name=pkg_name, pkg_encoded=pkg_encoded)
+def create_notebook_kernel(
+    notebook_body: str, pkg_name: str, pkg_encoded: str, enable_internet: bool = False
+):
+    bootstrap_code = create_bootstrap_code(
+        pkg_name=pkg_name, pkg_encoded=pkg_encoded, enable_internet=enable_internet
+    )
 
     notebook_obj = erace_all_outputs(json.loads(notebook_body))
 
-    bootstrap_cell = create_bootstrap_cell(pkg_name=pkg_name, pkg_encoded=pkg_encoded)
+    bootstrap_cell = create_bootstrap_cell(
+        pkg_name=pkg_name, pkg_encoded=pkg_encoded, enable_internet=enable_internet
+    )
     notebook_obj.setdefault("cells", []).insert(0, bootstrap_cell)
 
     notebook_kernel = json.dumps(notebook_obj)
