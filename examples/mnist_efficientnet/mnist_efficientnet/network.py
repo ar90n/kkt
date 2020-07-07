dabef432efee86a4f6d06f8a6d8d1827aeeef57e
Masahiro Wada <argon.argon.argon@gmail.com>
2020-07-07 20:43:54 +0900

    Update network

--- examples/mnist_efficientnet/mnist_efficientnet/network.py
+++ examples/mnist_efficientnet/mnist_efficientnet/network.py
@@ -1,21 +1,14 @@
 import timm
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 class Network(nn.Module):
     def __init__(self):
         super(Network, self).__init__()
-        self.backbone = timm.create_model('efficientnet_b0')
-        self.fc1 = nn.Linear(1000, 50)
-        self.fc2 = nn.Linear(50, 10)
+        self.net = timm.create_model('efficientnet_b0', num_classes=10)
 
     def forward(self, x):
-        x = self.backbone(x)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
+        return self.net(x)
 
 
 def extract_result(net_out):
